28c2d5c82600ca914809d1d51729a1405e938578
Marquinhus Goncalves <eu@marquinhusgoncalves.com>
2020-01-28 21:42:05 -0300

    chore: operacoes

---
+++ src/operacoes.kt
@@ -0,0 +1,35 @@
+// Seção 07 - Conceitos Iniciais
+
+fun main() {
+
+    println("2 + 2 = ${2 + 2}")
+    println("2 - 2 = ${2 - 2}")
+    println("2 / 2 = ${2 / 2}")
+    println("2 * 2 = ${2 * 2}")
+    println("10 % 4 = ${10 % 4}")
+    println("10 % 2 = ${10 % 2}")
+
+    var numero = 10
+
+    println("numero++ = ${numero++}") // numero = numero + 1
+    println("numero-- = ${numero--}")
+
+    println("++numero = ${++numero}")
+    println("--numero = ${--numero}")
+
+    numero += 2 // numero = numero + 2
+    println("numero += 2 = $numero")
+
+    numero -= 2
+    println("numero -= 2 = $numero")
+
+    numero /= 2
+    println("numero /= 2 = $numero")
+
+    numero *= 2
+    println("numero *= 2 = $numero")
+
+    numero %= 3
+    println("numero %= 3 = $numero")
+
+}
